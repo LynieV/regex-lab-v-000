a746266e6d07f068a3e6857f725a819d95ae6700
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-04-03 22:04:07 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,17 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aeiouAEIOU]/)
+    true
+  else word.match(/\b[a-zA-Z]/)
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+ text.scan(/\bun\w*\ling\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
